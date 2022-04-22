5a26b04064958445a6f93b0dc68940b7da00b0b4
Pablo <pablo.fju.pa@gmail.com>
2022-04-21 22:42:41 -0300

    'falta_retoques'

--- script.js
+++ script.js
@@ -7,63 +7,74 @@ let idSelecionado = {x: [], o: []};
 // a lista ordenada é validada, verificando todas possibilidades
 // 
 
+let placar = (vencedor)=>{
+
+    if(!placar.x && !placar.o){
+        placar.x = 0;
+        placar.o = 0;
+    }
+
+    if(vencedor==='x'){
+        placar.x += 1;
+        document.getElementById('x').innerText = placar.x
+    }else if(vencedor==='o'){
+        placar.o += 1;
+        document.getElementById('o').innerText = placar.o
+    }else{
+        console.log('[!] erro ao tentar detectar vencedor')
+    }
+}
+
 function* mudarCor(ids){
     this.ids = ids;
+    vezDeJogar ==='X'?placar('o'):placar('x');
+
     this.ids.map(valor=>{
         document.getElementById(valor).classList.add('quadrado_vencedor');
     })
+
     yield;
-    console.log('já')
     this.ids.map(valor=>{
         document.getElementById(valor).classList.remove('quadrado_vencedor');
     })
+
 }
 
 function validarVencedor(arr){ 
     
     if([1, 2, 3].every(value=>arr.includes(value))){
-        console.log(1);
         mudarCor.step = mudarCor([1, 2, 3]);
         mudarCor.step.next();
     }
     if([4, 5, 6].every(value=>arr.includes(value))){
-        console.log(true);
         mudarCor.step = mudarCor([4, 5, 6]);
         mudarCor.step.next();
     }
     if([7, 8, 9].every(value=>arr.includes(value))){
-        console.log(true);
         mudarCor.step = mudarCor([7, 8, 9]);
         mudarCor.step.next();
     }
     if([1, 4, 7].every(value=>arr.includes(value))){
-        console.log(true);
         mudarCor.step = mudarCor([1, 4, 7]);
         mudarCor.step.next();
     }
     if([2, 5, 8].every(value=>arr.includes(value))){
-        console.log(true);
         mudarCor.step = mudarCor([2, 5, 8]);
         mudarCor.step.next();
     }
     if([3, 6, 9].every(value=>arr.includes(value))){
-        console.log(true);
         mudarCor.step = mudarCor([3, 6, 9]);
         mudarCor.step.next();
     }
     if([1, 5, 9].every(value=>arr.includes(value))){
-        console.log(true);
         mudarCor.step = mudarCor([1, 5, 9]);
         mudarCor.step.next();
     }
     if([3, 5, 7].every(value=>arr.includes(value))){
-        console.log(true);
         mudarCor.step = mudarCor([3, 5, 7]);
         mudarCor.step.next();
     }
-
     
-
 }
 
 // remover clicajem, verificar vencedor, mudar status da vez do jogador
